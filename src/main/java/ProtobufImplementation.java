56faa9a6597fe015ed032470427784ff4b4b772a
Somya96 <sv1661@gmail.com>
2020-03-27 01:52:48 +0530

    protobuf implementation

---
+++ src/main/java/ProtobufImplementation.java
@@ -0,0 +1,39 @@
+import com.google.protobuf.ByteString;
+
+import generated.Person.Employee;
+import generated.Person.Employee.Address;
+import generated.Person.Employee.Nationality;
+
+public class ProtobufImplementation {
+
+    public static void main() {
+        try {
+            Employee employee = createEmployee();
+            ByteString empByteString = employee.toByteString();
+            System.out.println(empByteString);
+
+            Employee employee2 = Employee.parseFrom(empByteString);
+
+            System.out.println(employee2.getName());
+            System.out.println(employee2.getPhoneNumber());
+        } catch (Exception e) {
+
+        }
+    }
+
+    public static Employee createEmployee() {
+        Employee.Builder employee = Employee.newBuilder();
+        employee.setId(123);
+        employee.setName("Somya");
+
+        Employee.Address.Builder address = Address.newBuilder();
+        address.setCity("Hapur");
+        employee.setAddress(address);
+
+        employee.setNationality(Nationality.INDIAN);
+
+        employee.setPhoneNumber("8126503181");
+
+        return employee.build();
+    }
+}
